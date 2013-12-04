6088060db8e6b98838d4bf6dbf8cbdee576fdfff
kodeslacker <kodeslacker@kodeslackers-MacBook-Pro.local>
2013-12-04 22:26:19 +0200

    Fixes minor bugs

--- lib/smsBomber.rb
+++ lib/smsBomber.rb
@@ -4,6 +4,8 @@ class SmsBomber
 
   def initialize (modemAddress)
     @uart=SerialPort.new(modemAddress, 230400, 8, 1, SerialPort::NONE)
+    @uart.write('AT+CMGF=1'+13.chr)
+    sleep(1)
     @readingThread=Thread.new do
       while true do
         data=@uart.gets.chomp!
@@ -12,19 +14,21 @@ class SmsBomber
           print "Sent!\n"
         end
       end
+
     end
     puts 'Initializing modem in pdu mode.... OK!'
   end
 
   def sendFlashSMS(phoneNumber, smsText, smsCount)
+
     @uart.write('AT+CSMP=17,167,0,16'+13.chr)
     puts 'Setting modem to send class 0 SMS... OK!'
     sleep(2)
     sendMessage(phoneNumber, smsCount, smsText)
   end
   def sendSMS(phoneNumber, smsText, smsCount)
-    @uart.write('AT+CSMP=1,169,0,0'+13.chr)
-    puts 'Setting modem to send class 0 SMS... OK!'
+    @uart.write('AT+CSMP=17,167,0,0'+13.chr)
+    puts 'Setting modem to send SMS... OK!'
     sleep(2)
     sendMessage(phoneNumber, smsCount, smsText)
   end
@@ -36,11 +40,11 @@ class SmsBomber
       print "Sending Message ##{i}"
       @modemBusy=true
       @uart.write("AT+CMGS=\"#{phoneNumber}\""+13.chr)
-      sleep(0.1)
-      @uart.write(smsText+26.chr+13.chr)
+      sleep(1)
+      @uart.write(smsText+26.chr)
       while @modemBusy do
         print '.'
-        sleep(0.3)
+        sleep(1)
       end
     end
   end
--- smsBomber.gemspec
+++ smsBomber.gemspec
@@ -1,6 +1,6 @@
 Gem::Specification.new do |s|
   s.name        = 'smsBomber'
-  s.version     = '0.0.2'
+  s.version     = '0.0.4'
   s.date        = '2013-12-04'
   s.summary     = 'smsBomber'
   s.description = 'A simple gem that helps you connect to a modem and send SMS and Flash(class 0) messages'
